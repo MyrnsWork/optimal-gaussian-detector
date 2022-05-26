8a64daaa9889100e0ee5f0183c2cfc0f8c48bd35
MyrnsWork <laviec.mael@outlook.fr>
2022-05-26 22:27:36 +0200

    Ajout du type de cible Swerling 1 - V0
    - Ajout de la détection optimale des cibles de type swerling 1
    - Mise au propre : commentaires et concaténation de figures pour plus de clarté

--- matlab/CWMF.m
+++ matlab/CWMF.m
@@ -12,12 +12,12 @@ Pfa = 1e-4;                                                                % pro
 %% Paramètres
 % géométrie des cartes
 Nrec  = 64;                                                                % nombre de récurrences, 1x1
-Ncd   = 22000;                                                             % nombre de cases distance, 1x1
+Ncd   = 100;                                                               % nombre de cases distance, 1x1
 Ncell = Nrec * Ncd;                                                        % nombre de cellules, 1x1
 
 % radar
 fe     = 10e9;                                                             % fréquence de la porteuse, 1x1 [Hz]
-lambda = c / fe;                                                           % longueur d'onde de la porteuse, 1x1 [m]
+lambda = c/fe;                                                             % longueur d'onde de la porteuse, 1x1 [m]
 Bp     = 200e6;                                                            % bande passante, 1x1 [m]
 Li     = 1/Bp;                                                             % longueur d'impulsion, 1x1 [s]  
 PRF    = 2000;                                                             % fréquence de récurrence, 1x1 [Hz]
@@ -27,17 +27,17 @@ T_degCel = 20;                                                             % tem
 T_K      = 273.15 + T_degCel;                                              % température en degrés Kelvin, 1x1 [°K]
 F_dB     = 6;                                                              % facteur de bruit de l'électronique analogique, 1x1
 F_lin    = 10^( F_dB/10 );                                                 
-Pbth_lin = kB * T_K * Bp * F_lin;                                          % puissance du bruit thermique, 1x1 [W]   
-                                                                                                  
+Pbth_lin = kB * T_K * Bp * F_lin;                                          % puissance du bruit thermique, 1x1 [W]                                                                                                   
 Pbth_dB  = 10*log10( Pbth_lin );                                           
 R        = Pbth_lin * eye(Nrec);                                           % matrice de covariance du bruit thermique, Nrec x Nrec
 
 % caractéristiques de la cible
 SNR_dB          = 10;                                                      % rapport signal sur bruit, 1x1                                                                          
 SNR_lin         = 10^( SNR_dB/10 );
-typeTarget      = "deterministic";                                         % type de fluctuations de la cible, 1x1    
+typeTarget      = "swerling1";                                             % type de fluctuations de la cible, 1x1   
 speedTarget     = 5;                                                       % vitesse radiale de la cible, 1x1 [m/s]   
 targetFrequency = 2 * speedTarget / lambda;                                % fréquence Doppler de la cible, 1x1 [Hz]   
+
 
 %% Imagette de bruit blanc complexe (channels I et Q)
 [ imagetteChannelIQ_lin,...
@@ -48,14 +48,13 @@ targetFrequency = 2 * speedTarget / lambda;                                % fr�
                                            
 %% Création de la cible 
 [ targetIQ,...
-  amplitude,...
-  phase,...
-  steringVector ] = createTarget( SNR_lin,...
-                                  Pbth_lin,...
-                                  targetFrequency,...
-                                  PRF,...
-                                  typeTarget,...
-                                  Nrec               );
+  targetAmplitude,...
+  steringVector      ] = createTarget( SNR_lin,...
+                                       Pbth_lin,...
+                                       targetFrequency,...
+                                       PRF,...
+                                       typeTarget,...
+                                       Nrec               );
 
 %% Ajout de la cible
 [ imagetteChannelIQWithTarget_lin,...
@@ -65,40 +64,40 @@ targetFrequency = 2 * speedTarget / lambda;                                % fr�
                                                     targetIQ,...
                                                     1                        );                            
 
-%% CWMF
-logLRT_lin = CoherentWMF( imagetteChannelIQWithTarget_lin,...
-                          R,...
-                          targetIQ,...
-                          1                                  );    
-                      
-%% Detection des cibles
-[ detectionMap,...
-  gammaLogLRT_lin ] = CWMFdetector( logLRT_lin,...
-                                    R,...
-                                    targetIQ,...
-                                    Pfa           );     
+%% Détecteur optimal
+[ logLRT_lin,...
+  detectionMap,...
+  gammaLogLRT_dB  ] = optimalDetector( imagetteChannelIQWithTarget_lin,...
+                                       R,...
+                                       Pfa,...
+                                       targetIQ,...
+                                       steringVector,...
+                                       typeTarget,...
+                                       1                                  );    
 
-gammaLogLRT_dB = 10 * log10(gammaLogLRT_lin);  
 
 %% Figures
-fig1 = displayImagette( imagettePuissance_lin,...
-                        1                        );     
+fig1 = displayImagettes( imagettePuissance_lin,...
+                         imagettePuissanceWithTarget_lin,...
+                         detectionMap,...
+                         rangeIndex,...
+                         Nrec,...
+                         1                        );     
+
 
 fig2 = displayHistogram( imagetteAmplitude_lin,...
                          imagettePuissance_lin,...
                          Pbth_lin,...
                          2                        );
                      
-fig3 = displayImagette( imagettePuissanceWithTarget_lin,...
-                        3                                  );      
 
-fig4 = displayLogLRT( logLRT_lin,...
-                      4             );                 
 
-fig5 = displayDetectionMap( detectionMap,...
-                            rangeIndex,...
-                            Nrec,...
-                            5               );
+fig3 = displayLogLRT( logLRT_lin,...
+                      gammaLogLRT_dB,...
+                      Ncd,...
+                      3                 );                 
+
+
                       
 
 
@@ -116,24 +115,33 @@ function [ imagetteChannelIQ_lin,...
 end
 
 function [ targetIQ,...
-           amplitude,...
-           phase,...
-           steringVector ] = createTarget( SNR_lin,...
-                                           Pbth_lin,...
-                                           targetFrequency,...
-                                           PRF,...
-                                           type,...
-                                           Nrec               )
+           targetAmplitude,...
+           steringVector      ] = createTarget( SNR_lin,...
+                                                Pbth_lin,...
+                                                targetFrequency,...
+                                                PRF,...
+                                                type,...
+                                                Nrec               )
                                
                                
     switch type
         case "deterministic"
-            amplitude     = sqrt(SNR_lin * Pbth_lin);
-            phase         = 0;
-            t             = (0 : Nrec-1)' * 1/PRF;  
-            steringVector = exp( 1j*2*pi * targetFrequency * t);
-            targetIQ      = amplitude .* exp(1j .* phase) .* steringVector; 
-            
+            amplitude       = sqrt(SNR_lin * Pbth_lin);
+            phase           = 0;
+            targetAmplitude = amplitude .* exp(1j .* phase);
+            t               = (0 : Nrec-1)' * 1/PRF;  
+            steringVector   = exp( 1j*2*pi * targetFrequency * t);
+            targetIQ        = targetAmplitude .* steringVector; 
+
+        case "swerling1"
+            targetPower     = SNR_lin * Pbth_lin;
+            sigmaTarget     = sqrt( targetPower/2 );
+            targetAmplitude = sigmaTarget * ( randn + 1j*randn );          % hypothèse : le tirage d'ampltitude est 
+                                                                           % constant sur le temps d'intégration (Tint =9 Nrec/PRF )
+            t               = (0 : Nrec-1)' * 1/PRF;  
+            steringVector   = exp( 1j*2*pi * targetFrequency * t);
+            targetIQ        = targetAmplitude .* steringVector;   
+
         otherwise
             errror( "type de cible inconnu ou non supporté" );
     end
@@ -169,10 +177,15 @@ function [ imagetteChannelIQ_lin,...
    
 end
 
-function logLRT_lin = CoherentWMF( imagetteChannelIQ_lin,...
-                                   R,...
-                                   targetIQ,...
-                                   dimensionRec                       )
+function [ logLRT_lin,...
+           detectionMap,...
+           gammaLogLRT_dB  ] = optimalDetector( imagetteChannelIQ_lin,...
+                                                R,...
+                                                Pfa,...
+                                                targetIQ,...
+                                                steringVector,...
+                                                typeTarget,...
+                                                dimensionRec             )
                                
    if dimensionRec == 1                                                      
        Ncd = size(imagetteChannelIQ_lin, 2);
@@ -189,60 +202,63 @@ function logLRT_lin = CoherentWMF( imagetteChannelIQ_lin,...
    
    logLRT_lin = zeros( size(imagetteChannelIQ_lin) );
    for iRangeGate = (1  : Ncd)
-      
-      logLRT_lin(:, iRangeGate) = 2 * real(targetIQ' * (R \ imagetteChannelIQ_lin(:, iRangeGate)) );
+      switch typeTarget
+
+        case "deterministic"
+            logLRT_lin(:, iRangeGate) = 2 * real(targetIQ' * (R \ imagetteChannelIQ_lin(:, iRangeGate)) );
+            sigma0                    = sqrt( real( 2 * targetIQ' * (R \ targetIQ) ) );
+            gammaLogLRT_lin           = sigma0 * qfuncinv(Pfa);
+            gammaLogLRT_dB            = 10 * log10(gammaLogLRT_lin);  
+            
+        case "swerling1"
+            logLRT_lin(:, iRangeGate) = abs(steringVector' * (R \ imagetteChannelIQ_lin(:, iRangeGate)) ).^2;
+            sigma0                    = real( steringVector' * (R \ steringVector) );                           % partie réelle uniquement (à cause des erreurs numériques)
+            gammaLogLRT_lin           = -sigma0 * log( Pfa );
+            gammaLogLRT_dB            = 10 * log10(gammaLogLRT_lin);  
+
+        otherwise
+            errror( "type de cible inconnu ou non supporté" );
+
+      end
 
    end
 
-end
-
-function [ detectionMap,...
-           gammaLogLRT_lin ] = CWMFdetector( logLRT_lin,...
-                                             R,...
-                                             targetIQ,...
-                                             Pfa           )     
-
-
-    sigma0          = sqrt( real( 2 * targetIQ' * (R \ targetIQ) ) );
-    gammaLogLRT_lin = sigma0 * qfuncinv(Pfa);
-    
-    
     detectionMap = logLRT_lin >= gammaLogLRT_lin;
-    
+
 end
 
-function fig = displayImagette( imagettePuissance_lin,...
-                                iFigure                  )
+function fig = displayImagettes( imagettePuissance_lin,...
+                                 imagettePuissanceWithTarget_lin,...
+                                 detectionMap,...
+                                 rangeTarget,...
+                                 Nrec,...
+                                 iFigure                  )
 
     fig = figure(iFigure);
-    imagesc( 10*log10( abs(imagettePuissance_lin') ) );
+    set(fig, 'Units', 'Normalized', 'Position', [0 0 1 1]);
+    minPower = 10 * log10( min( min( imagettePuissance_lin(:) ), min( imagettePuissanceWithTarget_lin(:) ) ) );
+    maxPower = 10 * log10( max( max( imagettePuissance_lin(:) ), max( imagettePuissanceWithTarget_lin(:) ) ) );
+
+    %----------------------------------------------------------------------
+    subplot 131
+    imagesc( 10*log10( abs(imagettePuissance_lin') ), [minPower maxPower]);
     colorbar, colormap('parula')
     set(gca, 'YDir', 'normal')
     xlabel('Réccurence')
     ylabel('Distance relative')
-    title('Imagette de puissance [dB]')
+    title('Imagette de puissance sans cible [dB]')
+    
+    %----------------------------------------------------------------------
+    subplot 132
+    imagesc( 10*log10( abs(imagettePuissanceWithTarget_lin') ), [minPower maxPower]);
+    colorbar, colormap('parula')
+    set(gca, 'YDir', 'normal')
+    xlabel('Réccurence')
+    ylabel('Distance relative')
+    title('Imagette de puissance avec cible [dB]')
 
-
-end      
-
-function fig = displayLogLRT( logLRT_lin,...
-                              iFigure       )
-
-    fig = figure(iFigure);
-    plot( 10*log10( abs( logLRT_lin(1,:) ) ), 'LineWidth', 1.5 );
-    ylabel('Rapport du log-vraisemblance [dB]')
-    xlabel('Distance relative')
-    title('Evolution du rapport de log-vraisemblance [dB]')
-
-
-end      
-
-function fig = displayDetectionMap( detectionMap,...
-                                    rangeTarget,...
-                                    Nrec,...
-                                    iFigure                  )
-
-    fig = figure(iFigure);
+    %----------------------------------------------------------------------
+    subplot 133
     imagesc( detectionMap' ), hold on;
     plot( (1 : Nrec), rangeTarget * ones(1, Nrec), 'ro' ), hold off; 
     colorbar, colormap('parula')
@@ -250,6 +266,22 @@ function fig = displayDetectionMap( detectionMap,...
     xlabel('Réccurence')
     ylabel('Distance relative')
     title('Carte de détection')
+
+end      
+
+function fig = displayLogLRT( logLRT_lin,...
+                              gammaLogLRT_dB,...
+                              Ncd,...
+                              iFigure           )
+
+    fig = figure(iFigure);
+    plot( 10*log10( abs( logLRT_lin(1,:) ) ), 'LineWidth', 1.5 ), hold on;
+    plot( gammaLogLRT_dB * ones(1, Ncd), 'LineWidth', 1.5 ), hold off;
+    legend( "LogLRT", "Seuil Optimal" )
+    ylabel('Rapport du log-vraisemblance [dB]')
+    xlabel('Distance relative')
+    title('Evolution du rapport de log-vraisemblance [dB]')
+    set(fig, 'Units', 'Normalized', 'Position', [0 0 1 1]);
 
 
 end      
@@ -262,6 +294,7 @@ function fig = displayHistogram( imagetteAmplitude_lin,...
                              
                              
    fig = figure( iFigure );
+   set(fig, 'Units', 'Normalized', 'Position', [0 0 1 1]);
    subplot 121
    N    = 1000;
    Pmax = 1 - 1/length( imagetteAmplitude_lin(:) );
@@ -271,7 +304,7 @@ function fig = displayHistogram( imagetteAmplitude_lin,...
                                      
    histogram( imagetteAmplitude_lin(:), 'Normalization', 'pdf'), hold on;
    plot( x, fx, 'LineWidth', 1.5), hold off;
-   xlabel( "Amplitude [v]" );
+   xlabel( "Amplitude [V]" );
    ylabel( "Densité de probabilité" );
    title( "Densité de probabilité de l'imagette pour P_{bth}=" + string( round( 10*log10(Pbth_lin) ) ) + "dB" ); 
    grid on;
