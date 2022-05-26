3241c551fb84cf7ad3dc935934e81396c845c316
MyrnsWork <laviec.mael@outlook.fr>
2022-05-26 19:41:41 +0200

    Update CWMF.m

--- matlab/CWMF.m
+++ matlab/CWMF.m
@@ -5,38 +5,39 @@ clear;
 dbstop if error;
 
 %% Hyperparamètres
-kB  = 1.38e-23;
-c   = 3e8;
-Pfa = 1e-4;
+kB  = 1.38e-23;                                                            % constante de Boltzmann, 1x1 [m2 kg s-2 K-1]
+c   = 3e8;                                                                 % célérité de la lumière, 1x1 [m/s]
+Pfa = 1e-4;                                                                % probabilité de fausse alarme, 1x1
 
 %% Paramètres
-%géométrie des cartes
-Nrec  = 64;                  %nombre de récurrences, scalaire
-Ncd   = 22000;               %nombre de cases distances, scalaire
-Ncell = Nrec * Ncd;          %nombre de cellule, scalaires
+% géométrie des cartes
+Nrec  = 64;                                                                % nombre de récurrences, 1x1
+Ncd   = 22000;                                                             % nombre de cases distance, 1x1
+Ncell = Nrec * Ncd;                                                        % nombre de cellules, 1x1
 
-%radar
-fe            = 10e9;        %fréquence de la porteuse, scalaire [Hz]
-lambda_m      = c / fe;      %longueur d'onde de la porteuse, scalaire [m]
-Bp            = 200e6;       %bande passante, scalaire [m]
-Li            = 1/Bp;        %longueur d'impulsion, scalaire [s]  
-PRF           = 2000;        %fréquence de récurrence, scalaire [Hz]
+% radar
+fe     = 10e9;                                                             % fréquence de la porteuse, 1x1 [Hz]
+lambda = c / fe;                                                           % longueur d'onde de la porteuse, 1x1 [m]
+Bp     = 200e6;                                                            % bande passante, 1x1 [m]
+Li     = 1/Bp;                                                             % longueur d'impulsion, 1x1 [s]  
+PRF    = 2000;                                                             % fréquence de récurrence, 1x1 [Hz]
 
-%caractérisation du bruit thermique
-T_degCel = 20;               %température en degrés celsus
-T_K      = 273.15 + T_degCel;%température en degrés Kelvin
-F_dB     = 6;
-F_lin    = 10^( F_dB/10 );    %facteur de bruit de l'électronique analogique
-Pbth_lin = kB * T_K * Bp * F_lin;
-                             %Puissance du bruit thermique                          
-Pbth_dB  = 10*log10( Pbth_lin );                              
-R        = Pbth_lin * eye(Nrec);
+% caractérisation du bruit thermique
+T_degCel = 20;                                                             % température en degrés Celsus, 1x1 [°C]
+T_K      = 273.15 + T_degCel;                                              % température en degrés Kelvin, 1x1 [°K]
+F_dB     = 6;                                                              % facteur de bruit de l'électronique analogique, 1x1
+F_lin    = 10^( F_dB/10 );                                                 
+Pbth_lin = kB * T_K * Bp * F_lin;                                          % puissance du bruit thermique, 1x1 [W]   
+                                                                                                  
+Pbth_dB  = 10*log10( Pbth_lin );                                           
+R        = Pbth_lin * eye(Nrec);                                           % matrice de covariance du bruit thermique, Nrec x Nrec
 
-%caractéristiques de la cible
-SNR_dB          = 10;
+% caractéristiques de la cible
+SNR_dB          = 10;                                                      % rapport signal sur bruit, 1x1                                                                          
 SNR_lin         = 10^( SNR_dB/10 );
-speedTarget     = 5;
-targetFrequency = 2 * speedTarget / lambda_m;
+typeTarget      = "deterministic";                                         % type de fluctuations de la cible, 1x1    
+speedTarget     = 5;                                                       % vitesse radiale de la cible, 1x1 [m/s]   
+targetFrequency = 2 * speedTarget / lambda;                                % fréquence Doppler de la cible, 1x1 [Hz]   
 
 %% Imagette de bruit blanc complexe (channels I et Q)
 [ imagetteChannelIQ_lin,...
@@ -53,7 +54,7 @@ targetFrequency = 2 * speedTarget / lambda_m;
                                   Pbth_lin,...
                                   targetFrequency,...
                                   PRF,...
-                                  "deterministic",...
+                                  typeTarget,...
                                   Nrec               );
 
 %% Ajout de la cible
@@ -98,7 +99,9 @@ fig5 = displayDetectionMap( detectionMap,...
                             rangeIndex,...
                             Nrec,...
                             5               );
-                        
+                      
+
+
 %% Fonctions 
 function [ imagetteChannelIQ_lin,...
            imagetteAmplitude_lin,...
